77c543cab9ea2e0550320d2cc76ab751801d329b
Douglas Lima <douglimadasilva@gmail.com>
2024-11-25 20:28:50 -0300

    teste

---
+++ datadog-ingress.tf
@@ -0,0 +1,19 @@
+resource "kubectl_manifest" "test" {
+    yaml_body = <<YAML
+      apiVersion: networking.k8s.io/v1
+      kind: Ingress
+      metadata:
+        name: test-ingress
+        annotations:
+          nginx.ingress.kubernetes.io/rewrite-target: /
+      spec:
+        rules:
+        - http:
+            paths:
+            - path: /testpath
+              pathType: "Prefix"
+              backend:
+                serviceName: test
+                servicePort: 80
+    YAML
+}
